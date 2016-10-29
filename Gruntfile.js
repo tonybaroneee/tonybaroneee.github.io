f0bf9ed68d1cb5feeeba0f4cdab0c20bca3a9410
Anthony Barone <abarone@twitter.com>
2016-10-29 19:34:49 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,88 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// grunt-contrib-clean
+		clean: {
+			build: {
+				src: ['css/dist', 'js/dist']
+			}
+		},
+
+		// grunt-contrib-concat
+		concat: {
+			js: {
+				src: [
+					'lib/jquery/dist/jquery.js',
+					'lib/bootstrap/dist/js/bootstrap.js',
+					'js/*.js'
+				],
+				dest: 'js/dist/home.js',
+			}
+		},
+
+		// grunt-contrib-uglify
+		uglify: {
+			build: {
+				src: 'js/dist/home.js',
+				dest: 'js/dist/home.min.js'
+			}
+		},
+
+		// grunt-sass
+		sass: {
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'css/dist/home.css': 'css/home.scss'
+				}
+			} 
+		},
+
+		// grunt-autoprefixer
+		autoprefixer: {
+			dist: {
+				files: {
+					'css/dist/home.css': 'css/dist/home.css'
+				}
+			}
+		},
+
+		// grunt-contrib-watch
+		watch: {
+			options: {
+				livereload: true
+			},
+			scripts: {
+				files: ['js/**/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			sass: {
+				files: ['css/**/*.scss','sass/_partials/**/*.scss'],
+				tasks: ['sass:dist']
+			},
+			autoprefixer: {
+				files: ['css/**/*.scss'],
+				tasks: ['autoprefixer:dist']
+			},
+		}
+
+	});
+
+	// Load up grunt modules
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// What to kick off when 'grunt' is run
+	grunt.registerTask('default', ['clean', 'concat', 'uglify', 'sass:dist', 'autoprefixer', 'watch']);
+
+}; 
---
+++ README.md
@@ -0,0 +1,4 @@
+anthonyb.me
+===
+
+Code for my personal site. [Visit here](http://anthonyb.me).
